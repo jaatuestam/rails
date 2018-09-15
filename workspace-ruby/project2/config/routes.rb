5497b73ad971713deb9bb67d0f56a6b8a3d425e2
jorge <jorge@correo.com>
2018-09-15 13:25:12 -0500

    first commit

--- workspace-ruby/project2/config/routes.rb
+++ workspace-ruby/project2/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'users/registrations'}
 
   authenticate :user do
   	resources :cursos, only: [:new, :create, :edit, :update, :destroy]
@@ -10,5 +10,14 @@ Rails.application.routes.draw do
   resources :cursos
 
   root 'static_pages#home'
+
+  namespace :api do
+  	namespace :v1 do
+  		resources :users
+  		devise_scope :user do
+  			post 'authentication_tokens/create', to: "authentication_tokens#create"
+  		end
+  	end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
